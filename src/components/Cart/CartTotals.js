b57b7f0cb720bcb8bae719d189ef9531224c5f39
Chudimikelson <48757745+Chudimikelson@users.noreply.github.com>
2019-11-12 13:01:12 +0100

    Bug fix

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Deliveryfees from '../Deliveryfees';
+import Deliveryfees from '../deliveryfees';
 
 
 export default function CartTotals({value}) {
